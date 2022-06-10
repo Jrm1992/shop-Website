70cecd5e670d9c49d3e416649be7b85973b301ea
DESKTOP-9NAFSUS\Jose <jose_roberto_6@msn.com>
2022-06-10 14:06:33 -0300

    women shop v1.1

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,11 +14,26 @@ function getWomen() {
     const womenShop = document.createElement('div')
     const women = document.querySelector('.women')
     women.style.color = "#e67e34"
-    womenShop.className = 'women-shop col-sm-6 col-md-8 mb-3'
+    womenShop.className = 'women-shop row'
 
     for (i = 0; i < 6; i++){
-        const item = document.createElement('img')
-        item.src = `./assets/img/women/${i}.jpg`
+        const addCart = document.createElement('button')
+        const item = document.createElement('div')
+        const img = document.createElement('img')
+        img.src = `./assets/img/women/${i}.jpg`
+        addCart.innerHTML ='Add to Cart'
+        addCart.className = 'addCart'
+        item.className = 'item col-12 col-md-4 col-sm-6'
+        img.addEventListener("mouseenter", () => {
+            addCart.classList.add('show')
+            img.style.opacity = 0.5
+        })
+        img.addEventListener("mouseout", () => {
+            addCart.classList.remove('show')
+            img.style.opacity = 1
+        })
+        item.appendChild(img)
+        item.appendChild(addCart)
         womenShop.appendChild(item)
     }
 
