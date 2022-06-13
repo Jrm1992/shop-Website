57870a920e8651ccbc7f6da5fa456220eb68ba90
DESKTOP-9NAFSUS\Jose <jose_roberto_6@msn.com>
2022-06-13 12:50:01 -0300

    v1.2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,7 @@
 const main = document.querySelector('.main')
+const women = document.querySelector('.women')
+const men = document.querySelector('.men')
+const shoes = document.querySelector('.shoes')
 
 function reload() {
     document.location.reload(true);
@@ -12,8 +15,9 @@ function removerMain() {
 
 function getWomen() {
     const womenShop = document.createElement('div')
-    const women = document.querySelector('.women')
     women.style.color = "#e67e34"
+    men.style.color = "black"
+    shoes.style.color = "black"
     womenShop.className = 'women-shop row'
 
     for (i = 0; i < 6; i++){
@@ -39,3 +43,67 @@ function getWomen() {
 
     main.appendChild(womenShop)
 }
+
+function getMen() {
+    const menShop = document.createElement('div')
+    women.style.color = "black"
+    men.style.color = "#e67e34"
+    shoes.style.color = "black"
+    menShop.className = 'men-shop row'
+
+    for (i = 0; i < 6; i++){
+        const addCart = document.createElement('button')
+        const item = document.createElement('div')
+        const img = document.createElement('img')
+        img.src = `./assets/img/men/${i}.jpg`
+        addCart.innerHTML ='Add to Cart'
+        addCart.className = 'addCart'
+        item.className = 'item col-12 col-md-4 col-sm-6'
+        img.addEventListener("mouseenter", () => {
+            addCart.classList.add('show')
+            img.style.opacity = 0.5
+        })
+        img.addEventListener("mouseout", () => {
+            addCart.classList.remove('show')
+            img.style.opacity = 1
+        })
+        item.appendChild(img)
+        item.appendChild(addCart)
+        menShop.appendChild(item)
+    }
+
+    main.appendChild(menShop)
+}
+
+function getShoes() {
+    const shoeShop = document.createElement('div')
+    women.style.color = "black"
+    men.style.color = "black"
+    shoes.style.color = "#e67e34"
+    shoeShop.className = 'shoes-shop row'
+
+    for (i = 0; i < 6; i++){
+        const addCart = document.createElement('button')
+        const item = document.createElement('div')
+        const img = document.createElement('img')
+        img.src = `./assets/img/shoe/${i}.png`
+        addCart.innerHTML ='Add to Cart'
+        addCart.className = 'addCart'
+        item.className = 'item col-12 col-md-4 col-sm-6'
+        img.addEventListener("mouseenter", () => {
+            addCart.classList.add('show')
+            img.style.opacity = 0.5
+        })
+        img.addEventListener("mouseout", () => {
+            addCart.classList.remove('show')
+            img.style.opacity = 1
+        })
+        item.appendChild(img)
+        item.appendChild(addCart)
+        shoeShop.appendChild(item)
+    }
+
+    main.appendChild(shoeShop)
+}
+
+
